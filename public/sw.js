05319878e435d4c4f8da9f58e45723b1534a3cec
Pruthivi13 <mai.to.pruthivi@gmail.com>
2025-12-28 19:46:03 +0530

    feat: Add PWA configuration files (manifest.json, sw.js)

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+// Service Worker for Campus Connect
+const CACHE_NAME = 'campus-connect-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
